93f86c070a36c6836983612096165073623fadb3
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2012-09-05 03:49:18 -0400

    Renamed the debug stylesheet.

--- cssee.js
+++ cssee.js
@@ -1,8 +1,8 @@
 (function() {
-  var cssee = 'https://raw.github.com/gtramontina/cssee/master/cssee.css';
+  var cssee = 'https://raw.github.com/gtramontina/cssee/master/css.ee';
 
   var head = document.getElementsByTagName('head')[0];
-  head.innerHTML += '<link rel="stylesheet" href="'+cssee+'">';
+  head.innerHTML += '<link rel="stylesheet" href="'+cssee+'" type="text/css">';
 
   var existingClasses = document.body.getAttribute('class') || '';
   document.body.setAttribute('class', existingClasses + ' cssee-debug');
--- test/cssee.test.js
+++ test/cssee.test.js
@@ -1,7 +1,7 @@
 describe('cssee.js', function() {
 
   it('loads the external debugger stylesheet into the head', function() {
-    expect($('head > link[rel=stylesheet][href$="cssee.css"]')).to.not.be.empty();
+    expect($('head > link[rel=stylesheet][href$="css.ee"]')).to.not.be.empty();
   });
 
   it('adds a "cssee-debug" class to the body tag', function() {
