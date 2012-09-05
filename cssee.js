bfc4a6b575509dab18943526bfc217400a3256ca
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2012-09-05 03:06:57 -0400

    Simpler. Better.

--- cssee.js
+++ cssee.js
@@ -1,8 +1,8 @@
 (function() {
-  var cssee = 'http://raw.github.com/gtramontina/cssee/master/cssee.css';
+  var cssee = '//raw.github.com/gtramontina/cssee/master/cssee.css';
 
   var head = document.getElementsByTagName('head')[0];
-  head.innerHTML += '<link rel="stylesheet" href="'+cssee+'" />';
+  head.innerHTML += '<link rel="stylesheet" href="'+cssee+'">';
 
   var existingClasses = document.body.getAttribute('class') || '';
   document.body.setAttribute('class', existingClasses + ' debug');
